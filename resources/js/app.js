f0829b6a7835698621f2ef6e43dcefbda4f2613e
Maja <suryatmaja.dev@gmail.com>
2026-09-18 17:15:17 +0700

    fix: restore missing string quotes in Inertia page resolver (app.js)

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,7 @@ gsap.defaults({
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true });
-        return pages[./Pages/ + name + .svelte];
+        return pages['./Pages/' + name + '.svelte'];
     },
     setup({ el, App, props }) {
         mount(App, { target: el, props });
