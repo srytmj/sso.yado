374d1996ffc65c1b02f54c272d787125f88f6743
Maja <suryatmaja.dev@gmail.com>
2026-09-18 17:55:17 +0700

    fix: resolve Svelte module default in inertia setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,8 @@ gsap.defaults({
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true });
-        return pages['./Pages/' + name + '.svelte'];
+        let page = pages[./Pages/ + name + .svelte];
+        return page.default || page;
     },
     setup({ el, App, props }) {
         mount(App, { target: el, props });
