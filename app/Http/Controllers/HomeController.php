399a8201aa27a5287597b75076f4b49e56e394a7
Maja <suryatmaja.dev@gmail.com>
2026-09-18 17:20:44 +0700

    fix: remove bogus duplicate InertiaResponse import in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response as InertiaResponse;
-use Illuminate\InertiaResponse\InertiaResponse;
 
 class HomeController extends Controller
 {
